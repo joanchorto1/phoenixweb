56dad01703f1487b28d79bc5b0929bde29c25158
alumne <joanchorto1@iesebre.com>
2023-11-23 17:53:30 +0100

    updateSEO

--- src/components/components/home.js
+++ src/components/components/home.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Button,  Carousel } from 'react-bootstrap';
+import { Button, Carousel } from 'react-bootstrap';
 import Layout from "../layout/layout";
-import LogoVidal from "../img/tomasvidal.png"
-import LogoGastromar from "../img/Selecció_161.png"
+import LogoVidal from "../img/tomasvidal.png";
+import LogoGastromar from "../img/Selecció_161.png";
 
 const growthData = [
     { month: 'Mes 1', leads: 50 },
@@ -28,36 +28,32 @@ const Home = () => {
     return (
         <Layout>
             {/* Sección de Bienvenida */}
-            <section
-                className="mb-0"
-                style={{
-                    backgroundImage: 'url("https://media.licdn.com/dms/image/C4D12AQFvR6webwTsKw/article-cover_image-shrink_720_1280/0/1533823206296?e=2147483647&v=beta&t=wZN4Py-PYNTGQa6WCk1HVhivWC8SWcoIYFN1LdI1b9A")',
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    color: '#fff',
-                    width: '100%',
-                    height: '50vh',
-                    display: 'flex',
-                    alignItems: 'center',
-                }}
-            >
+            <section className="mb-0" style={{
+                backgroundImage: 'url("https://media.licdn.com/dms/image/C4D12AQFvR6webwTsKw/article-cover_image-shrink_720_1280/0/1533823206296?e=2147483647&v=beta&t=wZN4Py-PYNTGQa6WCk1HVhivWC8SWcoIYFN1LdI1b9A")',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                color: '#fff',
+                width: '100%',
+                height: '50vh',
+                display: 'flex',
+                alignItems: 'center',
+            }}>
                 <div className="container text-center">
                     <h2 className={"mb-4"}>Phoenix Group</h2>
+                    <h3>Expertos en Marketing Digital, Diseño Gráfico y Desarrollo Web en Tarragona</h3>
                     <p className="text-center">
-                        Bienvenidos a Phoenix Group, donde la creatividad y la innovación se encuentran.
-                        Somos una empresa dedicada al mundo del marketing, diseño gráfico y programación,
-                        fusionando habilidades para ofrecer soluciones integrales a nuestros clientes.
-                        En Phoenix, no solo creamos, sino que también conectamos ideas con resultados.
+                        Descubre el poder de Phoenix Group: tu socio estratégico para soluciones de marketing, diseño
+                        gráfico y desarrollo web en Tarragona. Fusionamos creatividad y tecnología para llevar tu
+                        negocio al siguiente nivel.
                     </p>
-                    <Button variant="outline-light" href={"/about"}>Saber más</Button>
+                    <Button variant="outline-light" href={"/about"}>Conoce más</Button>
                 </div>
             </section>
 
             {/* Carrusel de Logos */}
-
-            <section className="mb-5 text-center" style={{backgroundColor:'#2c3e50'}}>
-                <h2 className="mb-4 text-white" style={{backgroundColor:'#2c3e50'}}>Nuestra Fortaleza</h2>
-                <div className="row text-white" style={{backgroundColor:'#2c3e50'}}>
+            <section className="mb-5 text-center" style={{ backgroundColor: '#2c3e50' }}>
+                <h2 className="mb-4 text-white">Nuestra Fortaleza</h2>
+                <div className="row text-white" style={{ backgroundColor: '#2c3e50' }}>
                     <div className="col-md-8 offset-md-2">
                         <p>
                             En Phoenix Group, nos destacamos por nuestra capacidad para fusionar creatividad y tecnología.
@@ -72,6 +68,8 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+
+            {/* Sección de Clientes */}
             <section className="mb-5 text-center">
                 <h2 className="mb-4">Nuestros Clientes</h2>
                 <Carousel className="d-inline">
@@ -79,27 +77,30 @@ const Home = () => {
                         <img className="d-inline w-25 h25"
                              src={LogoVidal}
                              alt="Piscines Tomas Vidal SL" />
-                        {/* Agregar más elementos del carrusel según sea necesario */}
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="d-inline w-25"
                              src={LogoGastromar}   alt="Gastromar Tortosa" />
-                        {/* Agregar más elementos del carrusel según sea necesario */}
                     </Carousel.Item>
                 </Carousel>
-                {/*<Button  variant="outline-light" href={"/clients"} style={{ marginTop: '5%', backgroundColor: '#2c3e50' }}>Ver más clientes</Button>*/}
+                {/*<div style={{ backgroundColor: '#2c3e50', width: '100%', alignItems: 'center' }} className={'mb-4'}>*/}
+                {/*    <Button variant="outline-light" className="mt-4"><a href="/clients" style={{ color: "white" }}>Ver más clientes</a></Button>*/}
+                {/*</div>*/}
             </section>
-            <section className="mb-5 text-center text-white pb-3 pt-4" style={{backgroundColor:'#2c3e50'}}>
+
+            {/* Sección de Contacto */}
+            <section className="mb-5 text-center text-white pb-3 pt-4" style={{ backgroundColor: '#2c3e50' }}>
                 <h2 className="mb-4">¿Listo para Potenciar tu Negocio?</h2>
                 <p>
                     Descubre cómo Phoenix Group puede llevar tu empresa al siguiente nivel. Estamos aquí para ofrecerte
                     soluciones personalizadas que se alinean con tus objetivos comerciales.
                 </p>
-                <div style={{backgroundColor: '#2c3e50',width: '100%', alignItems:'center'}} className={'mb-4'}>
-                    <Button variant="outline-light" className="mt-4"><a href="/contact" style={{ color: "white" }}>Contacta ahora!</a></Button>
+                <div style={{ backgroundColor: '#2c3e50', width: '100%', alignItems: 'center' }} className={'mb-4'}>
+                    <Button variant="outline-light" className="mt-4"><a href="/contact" style={{ color: "white" }}>Contacta ahora</a></Button>
                 </div>
             </section>
 
+            {/* Sección de Crecimiento de Clientes */}
             <section className="mb-5 text-center">
                 <h2 className="mb-4 mt-3">Crecimiento de Clientes</h2>
                 <div className="row">
@@ -118,19 +119,19 @@ const Home = () => {
                     ))}
                 </div>
                 <div className="mt-3">
-                    <a style={{backgroundColor:'#2c3e50'}} href="/contact" className="btn btn-primary">
+                    <Button style={{ backgroundColor: '#2c3e50' }} href="/contact" className="btn btn-primary">
                         Contacta
-                    </a>
+                    </Button>
                 </div>
             </section>
 
-
-            <section className="text-center " style={{backgroundColor: '#2c3e50', paddingTop:'2%'}}>
+            {/* Sección de Servicios */}
+            <section className="text-center " style={{ backgroundColor: '#2c3e50', paddingTop: '2%' }}>
                 <h2 className="mb-4  text-white">Nuestros Servicios</h2>
                 <div className="row" >
                     <div className="col-md-12 mb-4">
-                        <div style={{backgroundColor: '#2c3e50'}} className="shadow p-3 mb-5 text-white rounded">
-                            <h5 className="mt-3">MARKETING</h5>
+                        <div style={{ backgroundColor: '#2c3e50' }} className="shadow p-3 mb-5 text-white rounded">
+                            <h5 className="mt-3">Marketing Digital</h5>
                             <p>
                                 Ofrecemos soluciones integrales de marketing digital, desde la gestión de redes sociales
                                 hasta estrategias avanzadas de generación de leads.
@@ -139,8 +140,8 @@ const Home = () => {
                     </div>
 
                     <div className="col-md-12 mb-4">
-                        <div style={{backgroundColor: '#2c3e50'}} className="shadow p-3 mb-5 text-white rounded">
-                            <h5 className="mt-3">DESARROLLO WEB</h5>
+                        <div style={{ backgroundColor: '#2c3e50' }} className="shadow p-3 mb-5 text-white rounded">
+                            <h5 className="mt-3">Desarrollo Web</h5>
                             <p>
                                 Desarrollamos software personalizado, implementamos sistemas ERP para una gestión
                                 empresarial eficiente y creamos aplicaciones web innovadoras.
@@ -149,8 +150,8 @@ const Home = () => {
                     </div>
 
                     <div className="col-md-12 mb-4">
-                        <div style={{backgroundColor: '#2c3e50'}} className="shadow p-3 mb-5 text-white rounded">
-                            <h5 className="mt-3">DISEÑO GRÁFICO</h5>
+                        <div style={{ backgroundColor: '#2c3e50' }} className="shadow p-3 mb-5 text-white rounded">
+                            <h5 className="mt-3">Diseño Gráfico</h5>
                             <p>
                                 Transformamos ideas en imágenes impactantes, desde el diseño de logos y carteles hasta
                                 la creación de contenido visual para redes sociales.
@@ -159,20 +160,18 @@ const Home = () => {
                     </div>
 
                     <div className="col-md-12 ">
-                        <div style={{backgroundColor: '#2c3e50'}} className="shadow p-3 mb-5 text-white rounded">
-                            <h5 className="mt-3">GESTIÓN DE EVENTOS</h5>
+                        <div style={{ backgroundColor: '#2c3e50' }} className="shadow p-3 mb-5 text-white rounded">
+                            <h5 className="mt-3">Gestión de Eventos</h5>
                             <p>
                                 Nos encargamos de la organización completa de eventos, desde la planificación hasta la
                                 ejecución, incluyendo la contratación de personal y el alquiler de infraestructuras.
                             </p>
                         </div>
                     </div>
-                    <div style={{backgroundColor: '#2c3e50',width: '100%', alignItems:'center'}} className={'mb-4'}>
+                    <div style={{ backgroundColor: '#2c3e50', width: '100%', alignItems: 'center' }} className={'mb-4'}>
                         <Button variant="outline-light" className="mt-4"><a href="/services" style={{ color: "white" }}>Saber más</a></Button>
                     </div>
-
                 </div>
-
             </section>
 
         </Layout>
